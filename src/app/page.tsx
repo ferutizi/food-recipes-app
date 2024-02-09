d6898c2fa4d485bdeebc911d8e6210504b336d86
ferutizi <fer.utizi@gmail.com>
2024-02-09 13:35:35 -0300

    added error text for not found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useEffect } from 'react'
+import './globals.css'
+import { useEffect, useState } from 'react'
 import useFetchRecipes from './hooks/useFetchRecipes'
 import Ingredient from './components/Ingredient'
 
@@ -23,7 +24,8 @@ export interface Recipe {
 }
 
 export default function Home() {
-	const [fetchData, recipes] = useFetchRecipes()
+	const [query, setQuery] = useState<string>('')
+	const [fetchData, fetchByName, recipes] = useFetchRecipes()
 
 	useEffect(() => {
 		fetchData()
@@ -33,19 +35,35 @@ export default function Home() {
 		console.log(recipes)
 	}, [recipes])
 
+	const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		fetchByName(query)
+	}
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className='text-3xl m-8'>What do you feel like having today</h1>
+			<section className='flex flex-col items-center'>
+				<h1 className='text-4xl font-semibold m-8'>What do you feel like having today</h1>
+				<form onSubmit={handleSubmit}>
+					<input 
+						className='m-8 w-96 h-10 rounded-2xl outline-none pl-4 text-black'
+						name='search'
+						type='text'
+						onChange={e => setQuery(e.target.value)}
+						autoFocus
+					/>
+				</form>
+			</section>
 			<section className='flex flex-wrap justify-center gap-12'>
-				{ recipes.length !== 0 ? (
+				{ recipes? (
 					recipes.map((e) => 
 						<article key={e.idMeal} className='w-96 bg-stone-100 rounded-2xl'>
 							<div className='h-72 rounded-2xl rounded-ee-none overflow-hidden'>
 								<img className='object-cover h-full w-full' src={e.strMealThumb} title={e.strMeal} alt={e.strMeal} />
-								<h2 className='text-2xl p-4 pt-2 pb-0 rounded-t-2xl text-black z-10 -translate-y-10 absolute bg-stone-100' key={e.idMeal} >{e.strMeal}</h2>
 							</div>
-							<div className='z-20'>
-								<div className='flex flex-wrap gap-2 p-4 z-20'>
+							<h2 className='text-2xl font-semibold p-4 pt-2 pb-2 rounded-t-2xl shadow-inset text-black -translate-y-10 absolute truncate w-72 bg-stone-100' key={e.idMeal} >{e.strMeal}</h2>
+							<div>
+								<div className='flex flex-wrap gap-2 p-4'>
 									<Ingredient ingredient={e.strIngredient1} />
 									<Ingredient ingredient={e.strIngredient2} />
 									<Ingredient ingredient={e.strIngredient3} />
@@ -55,7 +73,7 @@ export default function Home() {
 							</div>
 						</article>
 					))
-					: (<p>loading</p>)
+					: (<p>No recipe found for that food :C</p>)
 				}
 			</section>
 		</main>
