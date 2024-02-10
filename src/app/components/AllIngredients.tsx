2df41c665f0b05914387b83663a44a43840fe275
ferutizi <fer.utizi@gmail.com>
2024-02-10 19:58:52 -0300

    check if ingredient is not null to renderize in AllIngredients component

--- src/app/components/AllIngredients.tsx
+++ src/app/components/AllIngredients.tsx
@@ -13,14 +13,27 @@ export default function AllIngredients({e}: AllIngredientsProps) {
 	const [selectedRecipe, setSelectedRecipe] = useState<string>()
 
 	const showAllIngredients = (id: Recipe['idMeal']) => {
+		console.log(e)
 		setShowIngredients(true)
 		setSelectedRecipe(id)
+		console.log(id, selectedRecipe)
 	}
 
 	const hideAllIngredients = () => {
 		setShowIngredients(false)
 		setSelectedRecipe('')
 	}
+
+	const moreIngredients = [
+		e.strIngredient5,
+		e.strIngredient6,
+		e.strIngredient7,
+		e.strIngredient8,
+		e.strIngredient9,
+		e.strIngredient10,
+		e.strIngredient11,
+		e.strIngredient12,
+	]
 
 	return(
 		<div onMouseLeave={hideAllIngredients} className='flex flex-wrap gap-2 p-4'>
@@ -30,7 +43,7 @@ export default function AllIngredients({e}: AllIngredientsProps) {
 			<Ingredient ingredient={e.strIngredient4} />
 
 			{/* Hidden '...' for focused card that show all ingredients */}
-			{e.strIngredient5 !== '' && (!showIngredients || e.idMeal !== selectedRecipe) &&
+			{e.strIngredient5 !== '' && e.strIngredient5 !== null && (!showIngredients || e.idMeal !== selectedRecipe) &&
 					<p
 						onMouseEnter={() => showAllIngredients(e.idMeal)}
 						className={'m-0 px-2 py-1 bg- rounded-xl cursor-pointer text-white bg-black'}
@@ -39,14 +52,10 @@ export default function AllIngredients({e}: AllIngredientsProps) {
 			}
 			{showIngredients && e.idMeal === selectedRecipe &&
 				<>
-					<Ingredient ingredient={e.strIngredient5} />
-					<Ingredient ingredient={e.strIngredient6} />
-					<Ingredient ingredient={e.strIngredient7} />
-					<Ingredient ingredient={e.strIngredient8} />
-					<Ingredient ingredient={e.strIngredient9} />
-					<Ingredient ingredient={e.strIngredient10} />
-					<Ingredient ingredient={e.strIngredient11} />
-					<Ingredient ingredient={e.strIngredient12} />
+					{/* check if ingredient is not null to renderize */}
+					{moreIngredients.map((i, index) => i !== null && (
+						<Ingredient key={index} ingredient={i} />
+					))}
 				</>
 			}
 		</div>
