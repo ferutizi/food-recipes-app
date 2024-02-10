73f2373cb1face55e137b43ef359b56c0bd53b73
ferutizi <fer.utizi@gmail.com>
2024-02-10 18:15:15 -0300

    added types.ts file

---
+++ src/app/types/types.ts
@@ -0,0 +1,17 @@
+export default interface Recipe {
+	idMeal: string,
+	strMeal: string,
+	strMealThumb: string,
+	strIngredient1: string,
+	strIngredient2: string,
+	strIngredient3: string,
+	strIngredient4: string,
+	strIngredient5: string,
+	strIngredient6: string,
+	strIngredient7: string,
+	strIngredient8: string,
+	strIngredient9: string,
+	strIngredient10: string,
+	strIngredient11: string,
+	strIngredient12: string,
+}
