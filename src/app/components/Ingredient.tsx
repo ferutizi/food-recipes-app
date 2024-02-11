b9a671dc4d069d065059a7bba40c771f92fed26e
ferutizi <fer.utizi@gmail.com>
2024-02-11 18:00:25 -0300

    added title to ingredients truncate

--- src/app/components/Ingredient.tsx
+++ src/app/components/Ingredient.tsx
@@ -7,19 +7,21 @@ interface IngredientProps {
 export default function Ingredients({ingredient}: IngredientProps) {
 
 	function normalizeIngredient(ingr: string) {
-		// Reemplazar espacios con guiones bajos y convertir a minúsculas
+		// Replace spaces with underscores and convert to lowercase
 		return ingr.replace(/\s+/g, '_').toLowerCase()
 	}
 
 	const normalizedIngredient = normalizeIngredient(ingredient)	
 
 	const color = ingredients?.[normalizedIngredient]?.[0] || 'bg-black'
+	const border = ingredients?.[normalizedIngredient]?.[0] || 'border border-white'
 	const textColor = ingredients?.[normalizedIngredient]?.[1] || 'text-white'
 
 	return(
 		ingredient.length !== 0
 			? <p
-				className={`m-0 px-2 py-1 bg- rounded-xl truncate max-w-36 shadow-sm ${textColor} ${color}`}
+				title={ingredient}
+				className={`m-0 px-2 py-1 bg- rounded-xl truncate max-w-36 shadow-sm ${textColor} ${color} ${border}`}
 			>{ingredient}
 			</p>
 			: <></>
