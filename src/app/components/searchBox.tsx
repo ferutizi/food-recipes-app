1de689cb1f303f8e12c6bd8378d62209913068e1
ferutizi <fer.utizi@gmail.com>
2024-02-12 16:08:56 -0300

    fixed searchBar position input and button

--- src/app/components/searchBox.tsx
+++ src/app/components/searchBox.tsx
@@ -16,12 +16,13 @@ export default function SearchBox() {
 	}
 
 	return (
-		<form onSubmit={handleSubmit} className='flex flex-col items-center sm:inline-block'>
+		<form onSubmit={handleSubmit} className='flex flex-col items-center sm:inline-block sm:mb-8'>
 			<input
 				defaultValue={searchParams.get('q') || ''}
-				className='mx-0 mb-2 sm:mb-8 m-8 me-0 w-96 sm:w-72 md:w-96 h-10 rounded-2xl sm:rounded-e-none outline-none pl-4 text-black'
+				className='mx-0 mb-2 m-8 me-0 w-96 sm:w-72 md:w-96 h-10 rounded-2xl sm:rounded-e-none outline-none pl-4 text-black'
 				name="query"
 				placeholder='Search for your dream recipe...'
+				style={{transform: 'translate(0, 1px)'}}
 			/>
 			<button className='mb-8 w-96 sm:w-auto rounded-2xl h-10 border border-white sm:rounded-s-none px-2 hover:bg-stone-900 transition-all ease-in' type="submit">Search</button>
 		</form>
